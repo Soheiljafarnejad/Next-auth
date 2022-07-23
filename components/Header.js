ccc21a6b91b108e9c6c2c083b0ea929d8570e0ba
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-07-23 22:15:00 +0430

    Client-side Authentication - useSession

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,8 @@
 import Link from "next/link";
-import { signIn, signOut } from "next-auth/react";
+import { signIn, signOut, useSession } from "next-auth/react";
 const Header = () => {
+  const { data, status } = useSession();
+
   return (
     <header className="shadow bg-slate-100 px-4 py-2 flex items-center justify-between">
       <h1 className="self-center items-center">TodoList</h1>
@@ -11,8 +13,10 @@ const Header = () => {
         <Link href="/todos">
           <a>Todos</a>
         </Link>
-        <button onClick={() => signIn("github")}>sign in</button>
-        <button onClick={() => signOut()}>sign out</button>
+        {!data && status === "unauthenticated" && (
+          <button onClick={() => signIn("github")}>sign in</button>
+        )}
+        {data && status === "authenticated" && <button onClick={() => signOut()}>sign out</button>}
       </div>
     </header>
   );
