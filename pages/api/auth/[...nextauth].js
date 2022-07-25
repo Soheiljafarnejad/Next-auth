4f928c243377486da2110297ec9d782bbd8be70d
soheiljafarnejad <jafarnejad.official@gmail.com>
2022-07-25 22:22:36 +0430

    save user in Mongodb

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,11 +1,13 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
+import clientPromise from "@/server/lib/mongodb";
 export default NextAuth({
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
-    // ...add more providers here
   ],
+  adapter: MongoDBAdapter(clientPromise),
 });
